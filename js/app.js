867f013c3fc44075bc6b7c6fccced58b6acc99f8
John Brashear <jbrashear@edvoyles.com>
2017-11-08 14:40:07 -0500

    finished and tested

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
 const students = document.getElementsByClassName("student-item cf");
-const pages = Math.ceil(document.getElementsByClassName("student-item cf").length/10);
-
+const pages = Math.ceil(document.getElementsByClassName("student-item cf").length/10); //get total number of pages
+let html = "<ul>"; //variable to hold html for buttons
+//main function to display 10 items or break the loop if 10 not available.
 function displayPage(page) {
   let maxStudent = page * 10;
   let displayedStudent = maxStudent - 10;
-
   for ( let i = 0; i < students.length; i ++ ) {
     students[i].style.display = "none";
   }
@@ -17,4 +17,14 @@ function displayPage(page) {
   }
 }
 
-displayPage(1);
+displayPage(1); //display first 10 students
+//build up HTML
+for (let i = 0; i < pages; i++ ) {
+  j=i+1
+  html += "<li>";
+  html += '<a class="" href="#" onClick=displayPage(' +j+')>' + j + '</a>';
+  html += '</li>'
+}
+html += "</ul>";
+//display HTML in pagination div 
+document.getElementById("page").innerHTML = html;
